c8ab8eeb9d026799015864d8e3a85d7801a5b8d9
shahbazmobcast <shahbaz@mobcast.in>
2022-02-15 15:11:23 +0530

    check is number or not condition then return final zero

--- Addition.php
+++ Addition.php
@@ -5,7 +5,11 @@ function Addition($str){
         return "Negative Number not Allow";
     }
     preg_match_all('!\d+!', $str, $match);
-    return $match;
+    if(is_array($match[0])){
+        return $match[0];
+    }else{
+        return 0;
+    }
 }
 print_r(Addition("1,2"));
 ?>
