fed915e752f8cc94cc49bb76de60e715f578bbc6
shahbazmobcast <shahbaz@mobcast.in>
2022-02-15 15:05:21 +0530

    Negative Number not Allow
    Negative Number not Allow

--- Addition.php
+++ Addition.php
@@ -1,6 +1,9 @@
 <?php
 //addition of two string
 function Addition($str){
+    if(strpos($str,"-")!=""){
+        return "Negative Number not Allow";
+    }
     return $str;
 }
 echo Addition("1,2");
